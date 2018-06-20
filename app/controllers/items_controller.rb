0fc9eb040266d8e1367d892d9357ae163ce1fc24
BuffaloRob <peckham.rob@gmail.com>
2018-06-20 14:34:28 -0400

    initial version of Item controller, mirrored from V1 project

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,54 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+
+	def index
+		if current_user
+			@items = current_user.items
+		else
+			redirect_to new_user_session_path
+		end
+	end
+
+	def new
+		@item = Item.new
+	end
+
+	def create
+		@item = current_user.items.build(item_params)
+		if @item.save
+			redirect_to item_path(@item)
+		else
+			render :new
+		end
+	end
+
+	def update
+		@item.update(item_params)
+		redirect_to item_path(@item)
+	end
+
+	def destroy 
+		@item.destroy
+		redirect_to items_path
+	end
+
+	def show
+		#for use with form to make a new log
+		@log = @item.logs.build
+	end
+
+	def edit
+	end
+
+	private
+
+	def set_item 
+		@item = Item.find(params[:id])
+	end
+
+	def item_params
+		params.require(:item).permit(:name, :user_id)
+	end
+
+
 end
