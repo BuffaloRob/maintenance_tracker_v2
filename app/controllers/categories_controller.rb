fd68bac10611ef314cf3a13c8b67e30a0614e7c2
BuffaloRob <peckham.rob@gmail.com>
2018-06-20 16:09:21 -0400

    started work on index action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,11 @@
 class CategoriesController < ApplicationController
+
+  def index
+    if params[:item_id]
+      @item = Item.find_by(id: params[:item_id])
+      if @item.nil?
+        redirect_to item_path, alert: "That item does not exist"
+      else
+        
+  end
 end
