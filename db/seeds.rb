70c6de866039bc542e256e3ad186a4e2db11dd87
BuffaloRob <peckham.rob@gmail.com>
2018-06-22 09:32:17 -0400

    added seed info

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Item.destroy_all
+Category.destroy_all
+Log.destroy_all
+
+User.create(email: 'tom@tom.com', password: 'password', password_confirmation: 'password')
+
+Item.create(name: 'GT86', user_id: 1)
+
+Item.create(name: 'House', user_id: 1)
+
+Category.create(name: 'Oil Change', item_id: 1)
+
+Category.create(name: 'HVAC Filters', item_id: 2)
+
+Log.create(date_performed: 200.days.ago, date_due: Time.now, tools: 'wrench, rags, drip pan', cost: 40, notes: 'oil change note', category_id: 1, item_id: 1)
+
+Log.create(date_performed: 120.days.ago, date_due: 20.days.from_now, tools: 'screwdriver', cost: 20, notes: 'HVAC note', category_id: 2, item_id: 2)
