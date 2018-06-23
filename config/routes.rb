4d9c6e39a8d6bcaaf7d30753f0d3297f690e4aa5
BuffaloRob <peckham.rob@gmail.com>
2018-06-23 13:59:05 -0400

    added nested routes, past due, upcoming, and json

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
   resources :logs
   resources :categories
-  resources :items
+  resources :items do 
+    resources :logs, only: [:index, :show, :new, :edit, :create]
+    resources :categories, only: [:index, :show, :new, :edit, :create]
+  end
+  
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'past_due', to: "logs#past_due"
+  get 'upcoming', to: "logs#upcoming"
+
+  get 'logs/:id/log_data', to: 'logs#log_data'
 
   root "items#index"
 end
