af63fbd6dbb7e1828e4a41be9540cd235a6f035e
BuffaloRob <peckham.rob@gmail.com>
2018-06-25 15:24:08 -0400

    added formatted date helpers

--- app/helpers/logs_helper.rb
+++ app/helpers/logs_helper.rb
@@ -1,2 +1,10 @@
 module LogsHelper
+
+  def formatted_date_performed(log)
+    log.date_performed.strftime("%b %d, %Y")
+  end
+
+  def formatted_date_due(log)
+    log.date_due.strftime("%b %d, %Y")
+  end
 end
