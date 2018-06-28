5c63878033cbb5cf06e3054bcb80a0fe8db1ca51
BuffaloRob <peckham.rob@gmail.com>
2018-06-28 15:01:52 -0400

    copied from V1 project

---
+++ app/assets/javascripts/logs.js
@@ -0,0 +1,81 @@
+$(document).on('turbolinks:load', function () {
+
+  $("[id*=detailsBtn]").one("click", function(event) {
+    event.preventDefault();
+    let detailsPath = event.target.pathname;
+    debugger
+
+ 
+    // added http to this, only use 'detailsPath' if issues pop up
+    fetch(detailsPath, {
+      headers: {
+        credentials: 'same-origin'
+      }
+    })
+      .then(function (resp) {
+        //or specify resp.json()
+        return resp;
+      })
+      .then(function (data) {
+        const logDetails = new LogDetails(data.notes, data.tools);
+
+        $("#showDetails").append(logDetails.renderDetails());
+        console.log(logDetails.renderDetails());
+      });
+  });
+
+  $(".accordion").accordion({
+    collapsible: true,
+    active: false,
+  });
+
+  $("[id*=detailsBtn]").on("click", function (event) {
+    event.preventDefault();
+  });
+
+})
+
+class LogDetails {
+  constructor(notes, tools) {
+    this.notes = notes;
+    this.tools = tools;
+  }
+
+  renderDetails() {
+    console.log(`these are the deetz ${this.notes} and ${this.tools}.`)
+    return `
+      <h2>${this.notes}</h2>
+    `
+  }
+}
+
+//     $.getJSON(detailsPath)
+//       .done(function (data) {
+
+//         let result = "";
+//         const id = data.id;
+//         const note = data.notes;
+//         const date_performed = data.date_performed;
+//         const date_due = data.date_due;
+//         const tools = data.tools;
+//         const cost = data.cost;
+//         debugger
+//         const $details = $("#showDetails_" + id);
+
+//         result +=
+//           "<p>Performed on: " + date_performed + "</p>" + "\n" +
+//           "<p>Due on: " + date_due + "</p>" + "\n" +
+//           "<p>Notes: " + note + "</p>" + "\n" +
+//           "<p>Cost: $" + cost + "</p>" + "\n" +
+//           "<p>Tools: " + tools + "</p>" +
+//           "<hr>";
+
+//         $details.append(result);
+//       })
+//       .fail(function (data) {
+//         console.log("Error:");
+//         console.log(data);
+//       });
+//   });
+// })
+
