804aec9f31b6caa3f2cb9aa3292819168528329d
BuffaloRob <peckham.rob@gmail.com>
2018-06-29 14:27:29 -0400

    copied from V1 project

---
+++ app/assets/javascripts/items.js
@@ -0,0 +1,37 @@
+$(document).on('turbolinks:load', function () {
+
+  $("[id*=catBtn]").one("click", function (event) {
+    event.preventDefault();
+    let categoryPath = event.target.pathname;
+
+    $.getJSON(categoryPath)
+      .done(function (data) {
+        // console.log(data);
+        for (cat of data) {
+          let result = "";
+          let id = cat.item_id;
+          let name = cat.name;
+          let $category = $("#showCategory_" + id);
+
+          result += "<p>" + name + "</p>" + "<hr>";
+
+          $category.append(result);
+        }
+      })
+      .fail(function (data) {
+        console.log("Error:");
+        console.log(data);
+      });
+  });
+
+  $(".accordion").accordion({
+    collapsible: true,
+    active: false,
+  });
+
+  $("[id*=categoryBtn]").on("click", function (event) {
+    event.preventDefault();
+  });
+
+})
+
