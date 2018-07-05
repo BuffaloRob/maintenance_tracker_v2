d52f7d3672a61f778f8fc35dfbad1b57788624b1
BuffaloRob <peckham.rob@gmail.com>
2018-07-05 11:33:32 -0400

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,16 +11,20 @@ Item.destroy_all
 Category.destroy_all
 Log.destroy_all
 
-User.create(email: 'tom@tom.com', password: 'password', password_confirmation: 'password')
+User.create(email: 'tom@tom.com', password: 'password')
 
 Item.create(name: 'GT86', user_id: 1)
 
 Item.create(name: 'House', user_id: 1)
 
 Category.create(name: 'Oil Change', item_id: 1)
+Category.create(name: 'Rotate Tires', item_id: 1)
 
 Category.create(name: 'HVAC Filters', item_id: 2)
 
-Log.create(date_performed: 200.days.ago, date_due: Time.now, tools: 'wrench, rags, drip pan', cost: 40, notes: 'oil change note', category_id: 1, item_id: 1)
+Log.create(date_performed: 300.days.ago, date_due: 200.days.ago, tools: 'wrench, rags, drip pan', cost: 40, notes: 'Used Castrol 0W-20', category_id: 1, item_id: 1)
+Log.create(date_performed: 50.days.ago, date_due: 60.days.from_now, tools: 'wrench, rags, drip pan', cost: 50, notes: 'Used Motul 5w-30', category_id: 1, item_id: 1)
 
-Log.create(date_performed: 120.days.ago, date_due: 20.days.from_now, tools: 'screwdriver', cost: 20, notes: 'HVAC note', category_id: 2, item_id: 2)
+Log.create(date_performed: 200.days.ago, date_due: Time.now, tools: 'Lug Wrench', cost: 0, notes: 'Due in 10K miles', category_id: 2, item_id: 1)
+
+Log.create(date_performed: 120.days.ago, date_due: 20.days.from_now, tools: 'screwdriver', cost: 20, notes: 'Check in 3 months', category_id: 3, item_id: 2)
